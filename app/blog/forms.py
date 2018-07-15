e6e5a286327a324d0dca900c3856d5b5188f9e28
New <lionellloh@gmail.com>
2018-07-15 16:29:51 +0800

    Settled the ORM handling and rendering of blog posts

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -1,4 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, ValidationError
+from wtforms import PasswordField, StringField, SubmitField, ValidationError, TextField
 from wtforms.validators import DataRequired, Email, EqualTo
 
+class PostForm(FlaskForm):
+
+    title = StringField('Title', validators = [DataRequired()])
+
+    content = TextField('Post Content', validators = [DataRequired()])
+
+    submit = SubmitField('Post')
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,9 +1,37 @@
-from flask import render_template, request
-
+from flask import render_template, request, url_for, flash, redirect
+from flask_login import login_required
 from . import blog
+from .forms import PostForm
+from ..models import Post
 
 @blog.route("/")
 
-def blog():
+def blogposts():
 
-    return render_template('blog/index.html', title = "Blog")
+    posts = Post.query.all()
+    return render_template('blog/index.html', title = "Blog", posts=posts)
+
+@blog.route("/edit", methods =['GET', 'POST'])
+@login_required
+
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+
+        post = Post(title = form.title.data,
+                    content= form.content.data)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+            flash("Post added successfully")
+
+        except:
+            flash("Error: Post was not added")
+
+
+        return redirect(url_for("blog.blogposts"))
+    return render_template('blog/edit.html',
+                           title = "Write Posts",
+                           form = form)
